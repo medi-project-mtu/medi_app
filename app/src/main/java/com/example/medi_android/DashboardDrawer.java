9d69289e08d0eb7ce9e07d94a2e84047556d9295
yiming <y.tan@mycit.ie>
2021-10-23 18:57:25 +0100

    added medical data popup form upon clicking fab

--- app/src/main/java/com/example/medi_android/DashboardDrawer.java
+++ app/src/main/java/com/example/medi_android/DashboardDrawer.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.medi_android.databinding.ActivityDashboardDrawerBinding;
@@ -26,6 +27,8 @@ public class DashboardDrawer extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityDashboardDrawerBinding binding;
+    private AlertDialog.Builder dialogBuilder;
+    private AlertDialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,8 +41,9 @@ public class DashboardDrawer extends AppCompatActivity {
         binding.appBarDashboardDrawer.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+                createMDPopupForm();
             }
         });
         DrawerLayout drawer = binding.drawerLayout;
@@ -75,6 +79,32 @@ public class DashboardDrawer extends AppCompatActivity {
             builder.setMessage("Are you sure you want to log out?").setPositiveButton("Yes", dialogClickListener)
                     .setNegativeButton("No", dialogClickListener).show();
             return true;
+        });
+    }
+
+    private void createMDPopupForm() {
+        dialogBuilder = new AlertDialog.Builder(this);
+        final View formPopUpView = getLayoutInflater().inflate(R.layout.medical_data_form_popup, null);
+
+        Button popUpSave = (Button) formPopUpView.findViewById(R.id.md_form_popup_save);
+        Button popUpCancel = (Button) formPopUpView.findViewById(R.id.md_form_popup_cancel);
+
+        dialogBuilder.setView(formPopUpView);
+        dialog = dialogBuilder.create();
+        dialog.show();
+
+        popUpSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        popUpCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
         });
     }
 
